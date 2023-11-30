7dc534bfacb668179af7049a21458a2d1f5b07cd
yeon3ee <2310538@sookmyung.ac.kr>
2023-11-30 15:42:43 +0900

    02

--- main.c
+++ main.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-struct student {
-	int ID; //학번 
-	char name[10]; //이름 
-	double grade; //학점 
-	};
-	
-int main(void){
-	//instance 선언 
-	struct student s1 = {123, "cheyeon", 4.3};
+#include <math.h>
 
-	//다른 값 변경
-	s1.ID = 2310538;
-	strcpy(s1.name, "cheyeon");
-	s1.grade = 4.3;
-	//출력 
-	printf("ID : %d\n", s1.ID);
-	printf("name : %s\n", s1.name);
-	printf("grade : %f\n", s1.grade); 
-	
-	return 0;
+struct point {
+    int x;
+    int y;
+};
+
+int main(int argc, char *argv[]) {
+    struct point p1, p2;
+    int xdiff, ydiff;
+    double dist;
+
+    printf("input p1 coordinate (x y) : ");
+    scanf("%d %d", &p1.x, &p1.y);
+
+    printf("input p2 coordinate (x y) : ");
+    scanf("%d %d", &p2.x, &p2.y); // Fixed the typo here
+
+    xdiff = p2.x - p1.x;
+    ydiff = p2.y - p1.y;
+    dist = sqrt(xdiff * xdiff + ydiff * ydiff);
+
+    printf("distance : %f\n", dist);
+
+    return 0;
 }
-
 
 
 	
